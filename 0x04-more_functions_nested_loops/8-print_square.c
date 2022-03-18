c7494c2e7f5c15019ab315ca4133e4b0b5411362
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 05:28:39 +0000

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Print square.
+ *@size: variable
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int count1, count2;
+	count1 = 0;
+
+	if (size > 0)
+	{
+		while (count1 < size)
+		{
+			count2 = 0;
+			while (count2 < size)
+			{
+				_putchar('#');
+				count2++;
+			}
+			_putchar('\n');
+			count1++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
