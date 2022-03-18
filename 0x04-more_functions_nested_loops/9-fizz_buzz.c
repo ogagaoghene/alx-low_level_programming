674f9a0d17feea08c8962c3140b902e80a692a82
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 05:24:16 +0000

    Fizzbuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - fizzbuzz test
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else if (i != 100)
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
