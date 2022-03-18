7270d300f762503504d1a1cb6e3192dc69e81406
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 20:17:55 +0000

    9-fizz_buzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - primary function with for loop
- *
- * Description: for loop with if/else to print digits from 1 to 100
- * Return: na
+ * main - main block, solve fizz buzz from numbers 1 to 100
+ * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+ * Multiples of both 3 and 5 should print FizzBuzz.
+ * Return: 0
  */
 int main(void)
 {
-	int x;
+	int i;
 
-	for (x = 1; x < 101; x++)
+	i = 1;
+	while (i <= 100)
 	{
-		if (x % 3 == 0)
-		{
-			if (x % 5 == 0)
-				printf("FizzBuzz ");
-			else
-				printf("Fizz ");
-		} 
-		else if (x % 5 == 0)
-		{
-			if (x == 100)
-				printf("Buzz\n");
-			else
-				printf("Buzz ");
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%d ", x);
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
 	}
+	printf("\n");
+
 	return (0);
 }
+
