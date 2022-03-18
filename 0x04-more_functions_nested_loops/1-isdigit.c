34007b1c4d4f8e0d77433503da63a8d0331a0858
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 00:05:53 +0000

    1-isdigit prog

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for digit
+ *@c : variable
+ * Return: 0 or 1 .
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
