3f9e13d77bb04579f90fbe5bc138dca7ad7962be
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 18:08:54 +0000

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,30 @@
-#include "main.h"
 #include <stdio.h>
 
+/**
+* main - entry point
+* Return: void
+*/
+
 int main(void)
-{	
+{
 	int index;
-	for (index = 1; index <= 100; index++)
+
+	for (index = 1; index <= 100; index++)  
 	{
-		if ((index % 3) == 0) && ((index % 5) == 0)
+		if ((index % 3 == 0) && (index % 5 == 0))
 		{
-			printf("FizzBuzz\t");
-		}
-		else if (index % 3 == 0)
-		{
-			printf("Fizz\t");
+			printf("FizzBuzz");
 		}
 		else if (index % 5 == 0)
 		{
-			printf("Buzz\t");
+			printf("Buzz");
+		}
+		else if (index % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else 
-		{
-			printf("%d", index);
-		}
-		if (index != 100)
-		{
-			printf(" ");
-		}
+			printf("\n");	
 	}
-	return (0);
+	return (0);		
 }
