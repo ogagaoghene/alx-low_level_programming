ad46b59c77a676b5e041cb8b72b71fe0ed17d982
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-18 15:30:51 +0000

    Prints more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers between 0 to 14 incl.
+ *
+ * Return: Void.
+ */
+void more_numbers(void)
+{
+	int i;
+	int iterations;
+
+	for (iterations = 0; iterations < 10; iterations++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+			i++;
+		}
+	_putchar('\n');
+	}
+}
