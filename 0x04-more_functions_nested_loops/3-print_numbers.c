409a025d3ea965facbd010661c07d540162fdcbc
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-19 20:44:27 +0000

    Print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ * Return: void .
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar (i + '0');
+	}
+	_putchar('\n');
+}
+
