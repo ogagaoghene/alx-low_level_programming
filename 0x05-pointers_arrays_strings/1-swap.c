4390fd63d48ac716a40d904701bbbac836818791
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-21 20:50:26 +0000

    Function to swap to integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps the values of the two integers.
- * @a: variable
- * @b: variable
- */
+* swap_int: Function to swap two integers
+* @a: variable 
+* @b: variable
+*/
 
 void swap_int(int *a, int *b)
 {
-	int *temp;
-	*temp = *b;
-	*b = *a;
-	*a = *temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
-
