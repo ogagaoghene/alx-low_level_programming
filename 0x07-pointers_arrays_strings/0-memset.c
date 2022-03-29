362bf589672c234869b8286e572d7a47f156190b
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-29 10:05:04 +0000

    Function that fills memory with a const byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - Function that fills memory with a constant byte
+ * @dest: destination of copy
+ * @src: memory area to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
