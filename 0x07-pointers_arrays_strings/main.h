2add9b1580794e0dbaf3a557e751dd5c028b0c43
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-29 11:09:46 +0000

    Function Prototypes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
