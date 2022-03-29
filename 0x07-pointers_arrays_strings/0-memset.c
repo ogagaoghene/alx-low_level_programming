3b0075b7a2a5ae01ac37fb0d86b89f97af2eaa0c
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-29 11:02:01 +0000

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,22 @@
 #include "main.h"
+
 /**
- * _memcpy - Function that fills memory with a constant byte
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
+ * _memset - function that fills memory with a constant byte
+ * @s : the address of the memory we want to filled it u
+ * @b : the caracter that will fill the memory
+ * @n : the number of times we will fill the memory
+ * Return: memory.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
+
