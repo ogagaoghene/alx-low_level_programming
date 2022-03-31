d1cc625ca31dd6d18dd040845dbfd70f05a81d24
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-31 20:06:48 +0000

    Prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,18 @@
-#include "holberton.h"
-#include <string.h>
+#include "main.h"
 /**
- * _print_rev_recursion - check the code for Holberton School students.
- *@s : parameter
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
