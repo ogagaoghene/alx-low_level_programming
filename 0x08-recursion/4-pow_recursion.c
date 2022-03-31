50d6e801f7ab49998745debdc53d4b8cd53d3f4f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-31 00:38:10 +0000

    pow-recursion func

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,13 +7,13 @@
   *
   * Return: the value multiplied y times
   */
-int _pow_recursion (int x, int y)
+int _pow_recursion( int x, int y )
 {
-	if (y < 0)
-		return (-1);
+	if ( y < 0 )
+		return ( -1 );
 
-	if (y == 0)
-		return (1);
+	if ( y == 0 )
+		return ( 1 );
 
-	return (x * _pow_recursion(x, y - 1));
+	return ( x * _pow_recursion( x, y - 1 ));
 }
