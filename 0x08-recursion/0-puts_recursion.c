00876fc47137dc791deaa46fe4d3e3a0f4407a9c
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-03-31 00:07:32 +0000

    Prints a string followed by \n

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
