6c4356d1d487b9bfafec7706aa02fdc5e176d7f1
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-04-01 19:07:44 +0000

    Print the number of arguments passed to it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- *main - prints the name of the prints the name of the program
- *@argc: number of arguments passed to the function
- *@argv: argument vector of pointers to strings
- *
- *Return: always 0
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
-
