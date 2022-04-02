0a5776f70e84242a9cd8ce7bb38672b1dfcab723
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-04-02 00:10:00 +0000

    2-strchr

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -8,16 +8,17 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 	return (0);
 }
-
