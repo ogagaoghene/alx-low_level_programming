cf55a59fe6ad70a6192bdf5634d58ea4e13de2f8
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-04-07 23:17:35 +0000

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ *malloc_checked - unction that allocates memory using malloc.
+ *@b: unsigned int
+ *Return: VOID Pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+/* We don't know the type of bytes that will*/
+/*occupy the allocated memory so we use VOID pointer type */
+	void *pointer;
+
+/* Dynamic memory allocation using malloc function */
+	pointer = malloc(b);
+
+/* if malloc fails to allocate the needed memory */
+	if (pointer == NULL)
+/* exit status value */
+	exit(98);
+/* the function return a pointer */
+	return (pointer);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+#malloc, calloc, realloc, free
+
+#Description
+malloc, calloc, realloc, and free are essentially useful functions for allocating memory dynamically and automatically.
